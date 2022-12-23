fda095febdb898822dbe927aed8cf47ad870f918
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-24 02:45:00 +0430

    Updated Classes

--- student.rb
+++ student.rb
@@ -1,15 +1,20 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown')
+  def initialize(age, name = 'Unknown')
     ssuper(age, name)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
 end
