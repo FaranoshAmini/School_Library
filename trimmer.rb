10a4d6e13d43e7fa34cbcc8ae71f426401f99022
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-24 01:24:24 +0430

    Create trimmer.rb

---
+++ trimmer.rb
@@ -0,0 +1,12 @@
+require "./decorator.rb"
+
+class TrimmerDecorator < Decorator
+    def initialize(person)
+        @person = person
+        super(person)
+    end
+
+    def correct_name
+        @person.correct_name.slice(0,9)
+    end
+end
