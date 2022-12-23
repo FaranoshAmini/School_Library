0a341e39543ce0ab13fb3590919782e27278705d
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-23 22:53:54 +0430

    Create person.rb

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..9999)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    true if of_age?
+  end
+
+  private :of_age?
+end
