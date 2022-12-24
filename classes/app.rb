811c9da1e2e248068372fddc8e3f461470a391e9
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-25 00:03:59 +0430

    Finished adding UI

--- classes/app.rb
+++ classes/app.rb
@@ -6,7 +6,7 @@ require_relative './student'
 require_relative './teacher'
 require 'date'
 
-class APP
+class App
   def initialize
     @rentals = []
     @people = []
@@ -120,12 +120,12 @@ class APP
     else
       puts 'Select a book from the following list by number'
       @books.each_with_index { |book, index| puts "#{index}) Book Title: #{book.title}, Author: #{book.author}" }
-      rental_book = gets.chomp.to_1
+      rental_book = gets.chomp.to_i
       puts 'Select a person from the following list by number (not id)'
       @people.each_with_index do |person, index|
         puts "#{index}) Name: #{person.name} Age: #{person.age} Id: #{person.id}"
       end
-      rental_person = gets.chomp.to_1
+      rental_person = gets.chomp.to_i
       puts 'Enter date'
       date = convert_date(gets)
       rental_detail = Rental.new(@people[rental_person], @books[rental_book], date)
